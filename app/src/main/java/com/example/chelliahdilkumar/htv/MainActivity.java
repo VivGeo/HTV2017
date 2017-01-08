98331b249908555569a60963efa6c97e3d0bf128
VivGeo <vivek.george@ryerson.ca>
2017-01-08 09:26:20 -0500

    added map activity and circle/marker on desired destination todo: fix adjustable radius seekbar, background tasks to find distance (using Distance Matrix API)

--- app/src/main/java/com/example/chelliahdilkumar/htv/MainActivity.java
+++ app/src/main/java/com/example/chelliahdilkumar/htv/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.chelliahdilkumar.htv;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +9,17 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.io.IOException;
+import java.net.URL;
+
+
 
 public class MainActivity extends AppCompatActivity {
+
+    public final static String EXTRA_MESSAGE = "com.hashbrowns.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +36,19 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+
+
     }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, MapsActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
---
+++ app/src/main/java/com/example/chelliahdilkumar/htv/MapsActivity.java
@@ -0,0 +1,88 @@
+package com.example.chelliahdilkumar.htv;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.net.*;
+import java.io.*;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    String address;
+    double latitude;
+    double longitude;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+        URL u;
+        Intent intent = getIntent();
+        address = intent.getStringExtra(MainActivity.EXTRA_MESSAGE).replace(' ', '+');
+        try {
+            u = new URL("https://maps.googleapis.com/maps/api/geocode/json?address=" + address + "&key=AIzaSyDRxdp-_-cnS0z4KTu7JzeeGiz08zO4Drg");
+            URLConnection conn = u.openConnection();
+            InputStream is = conn.getInputStream();
+            InputStreamReader reader = new InputStreamReader(is);
+            BufferedReader br = new BufferedReader(reader);
+            String string = "";
+
+            while (br.ready()){
+                string += br.readLine();
+            }
+            int position = string.indexOf("\"lat\"");
+            latitude = Double.parseDouble(string.substring(position+7, position+18));
+            position = string.indexOf("\"lng\"");
+            longitude = Double.parseDouble(string.substring(position+7, position+18));
+
+        }
+        catch (Exception e)
+        {
+            System.out.println("yikes that isn't supposed to happen");
+            latitude = 43.7837405;
+            longitude = -79.187683;
+        }
+
+            // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng marker = new LatLng(latitude, longitude);
+        mMap.addMarker(new MarkerOptions().position(marker).title("Marker in location"));
+        Circle circle = mMap.addCircle(new CircleOptions().center(new LatLng(latitude,longitude)).radius(300).strokeColor(Color.argb(200,0,0,200)).fillColor(Color.argb(50,0,0,200)));
+        mMap.moveCamera(CameraUpdateFactory.zoomTo(15));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(latitude,longitude)));
+
+
+    }
+}
